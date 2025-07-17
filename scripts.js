0091b68c58cf314b493085184a40c6c0a95fab02
GMuchoki <muchokigeoffreymunene@gmail.com>
2025-07-17 13:53:12 +0300

    Completed the  mini practice to make localStorage
    Functions:
    - Load saved name on page load
    - Live update the <span> as user types
    - Save name to localStorage when button is clicked
    -  Clear input, output, and localStorage

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const nameInput = document.getElementById('nameInput');
+const spanId = document.getElementById('output');
+const saveToLocalStorage = document.getElementById('saveBtn');
+const clearButton = document.getElementById('clearBtn');
+
+// Load saved name on page load
+window.addEventListener('load', function loadSavedItem (){
+    const user = localStorage.getItem('username');
+    if (user){
+        nameInput.value = user;
+        spanId.textContent = user;
+    }
+});
+
+// Live update the <span> as user types
+nameInput.addEventListener('input', function () {
+    spanId.textContent = nameInput.value;
+});
+
+// Save name to localStorage when button is clicked
+saveToLocalStorage.addEventListener('click', function toLocalStorage (){
+    localStorage.setItem('username', spanId.textContent)
+});
+
+// Clear input, output, and localStorage
+clearButton.addEventListener('click', () => {
+    nameInput.value = '';
+    spanId.textContent = 'stranger';
+    localStorage.removeItem('username');
+});
